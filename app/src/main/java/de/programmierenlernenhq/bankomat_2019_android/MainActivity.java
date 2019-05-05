1e6d8bf9c16a0d842a0041da2ea26ba7eaa0e283
Ihor <panchenko.ihor84@gmail.com>
2019-05-05 20:38:28 +0200

    Zweite Schrit

--- app/src/main/java/de/programmierenlernenhq/bankomat_2019_android/MainActivity.java
+++ app/src/main/java/de/programmierenlernenhq/bankomat_2019_android/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnDeutsch = (Button) findViewById(R.id.btnDeutsch);
